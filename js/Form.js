de9819a9ca81f2b029968c825a9b082ffb8bca9c
hanieliraBR <128418691+hanieliraBR@users.noreply.github.com>
2023-07-17 15:15:44 -0300

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,29 @@
+class Form {
+  constructor() {
+ this.input = createInput("").attribute("placeholder", "digite seu nome");
+ this.playButton = createButton("play");
+this.tittleImg = createImg("./assets/title.png", "titulo do jogo");
+this.greeting = createElement("h2");
+}
+
+
+  setElementPosition() {
+    // DEFINA A POSIÇÃO DOS ELEMENTOS
+  }
+
+
+  hide() {
+   // USE A FUNÇÃO PARA DESAPARECER OS ELEMENTOS
+  }
+
+  handleMousePressed() {
+   // CRIE A FUNÇÃO DE PRESSIONAMENTO DE TECLA
+
+   /* O símbolo usado para escrever a mensagem abaixo (laranja) é a crase. */
+  }
+
+  display() {
+    // CHAME A FUNÇÃO DE POSIÇÃO DOS ELEMENTOS
+    // CHAME A FUNÇÃO DE PRESSIONAMENTO DE TECLA
+  }
+}
